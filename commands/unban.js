0a5576a7afd7becdd695045fd0188f2c3db206f5
LeGarsFou <benassisminimoi@gmail.com>
2022-02-10 10:32:42 +0100

    Il va falloir que je trouve une solution viable pour le unban, elle est donc tjrs "en développement"

--- commands/unban.js
+++ commands/unban.js
@@ -3,38 +3,37 @@ const Discord = require('discord.js'),
 
 module.exports = {
     run: async (message, args) => {
-        /*if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send(new Discord.MessageEmbed()
+        /*if (!message.author.hasPermission('BAN_MEMBERS')) return message.channel.send(new Discord.MessageEmbed()
         .setDescription('Vous n\'avez pas la permission d\'utiliser cette commande !')
         .setColor('#ff0000')
         .setTimestamp()
         .setThumbnail('https://cdn.discordapp.com/attachments/738864387019767921/821457688172167198/Hnet.com-image.gif'))
-        const member = message.mentions.members.first()
+        const member = args[0]
         if (!member) return message.channel.send(new Discord.MessageEmbed()
-        .setDescription('Veuillez mentionner le membre à unban.')
+        .setDescription('Veuillez donner l\'id de l\'utilisateur à unban.')
         .setColor('#ff0000')
         .setTimestamp()
         .setThumbnail('https://cdn.discordapp.com/attachments/738864387019767921/821457688172167198/Hnet.com-image.gif'))
-        if (message.member.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send('Vous ne pouvez pas unban ce membre.')
+        if (message.author.roles.highest.comparePositionTo(member.roles.highest) < 1 && message.author.id !== message.guild.ownerID) return message.channel.send('Vous ne pouvez pas unban ce membre.')
         const reason = args.slice(2).join(' ') || 'Aucune raison fournie'
-        await message.mentions.members.first().send(new Discord.MessageEmbed()
-        .setAuthor(`[UNBAN] sur le serveur ${message.guild.name}`, message.guild.iconURL())
-        .addField('Par le Modérateur', message.author, true)
-        .addField('Pour la Raison', reason, true)
-        .addField('Durée', humanizeDuration(duration, {language: 'fr'}), true)
-        .setColor('#00ff00')
-        .setTimestamp())
-        await message.guild.members.unban(member)
-        message.channel.send(new Discord.MessageEmbed()
+        try {
+            message.guild.fetchBans().then( bans => {
+                message.guild.members.unban(member)
+            })
+            await message.channel.send(new Discord.MessageEmbed()
             .setDescription(`${member.user.tag} a été débanni(e) !`)
             .setColor('#00ff00')
             .setTimestamp())
-        message.guild.channels.cache.get(config.logs).send(new Discord.MessageEmbed()
+            message.guild.channels.cache.get(config.logs).send(new Discord.MessageEmbed()
             .setAuthor(`[UNBAN] ${member.user.tag}`, member.user.displayAvatarURL())
             .addField('Utilisateur', member, true)
             .addField('Raison', reason, true)
             .setColor('#00ff00')
-            .setTimestamp())*/
-        message.channel.send("Commande en développement")
+            .setTimestamp())
+        } catch (err) {
+            message.channel.send('Une erreur est survenue lors du unban de l\'utilisateur : ' + err)
+        }*/
+        message.reply('Commande en développement !')
     },
     name: 'unban',
     guildOnly: true,
