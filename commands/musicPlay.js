c7002ef7ae5990a02fccb89716439e91ad687946
LeGarsFou <benassisminimoi@gmail.com>
2022-03-24 22:52:49 +0100

    Removed unnecessary vars in meme.js

--- commands/musicPlay.js
+++ commands/musicPlay.js
@@ -1,14 +1,14 @@
-const Discord = require('discord.js')
+require('discord.js');
 const ytdl = require('ytdl-core')
 
 var list = []
 
 module.exports = {
-    run: async (message, args) => {
+    run: async (message) => {
         if(message.member.voice.channel){
             let args = message.content.split(" ")
 
-            if(args[1] == undefined || !args[1].startsWith("https://www.youtube.com/watch?v=")){
+            if(args[1] === undefined || !args[1].startsWith("https://www.youtube.com/watch?v=")){
                 message.reply("Lien invalide ou mal fourni.")
             }
             else {
