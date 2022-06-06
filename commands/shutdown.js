8a76e9924ac67226f81bf9d901db7b2449ac911e
LeGarsFou <benassisminimoi@gmail.com>
2022-06-06 12:52:03 +0200

    Updated shutdown.js.

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -11,13 +11,13 @@ module.exports = {
         else message.channel.send(new Discord.MessageEmbed()
             .setDescription(`Le bot s'éteint...`)
             .setColor('#00ff00')
-            .setTimestamp()),
+            .setTimestamp())
             client.destroy()
     },
     name: 'shutdown',
     guildOnly: true,
     help: {
-        description: 'Cette commande permet d\'éteindre le bot. Il faut avoir la permission \"ADMINISTRATOR\" pour utiliser cette commande !',
+        description: `Cette commande permet d'éteindre le bot. Il faut être ${config.ownerPing} pour utiliser cette commande !`,
         category: "Autre",
         syntax: ''
     }
